0aad41a38020f07d306b36dc8204a318b540217a
Cristian Sagula <cristian@Cristians-MacBook-Pro.local>
2025-06-01 11:11:13 -0300

    initial setup

---
+++ components/Form/Form.tsx
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <div className="bg-primary-purple">
+      <div className="rounded-tl-[32px] rounded-bl-[32px] bg-white h-full">
+        <h1>USPS Shipping</h1>
+        <p>Print USPS shipping labels with ease</p>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
